855526787847a8075ed71b41cb1c603d7307aa54
Niels Nielsen <niels.nielsen@gmail.com>
2024-05-22 20:15:56 -0600

    Initial project setup with next.js, typescript, eslint and prettier

---
+++ src/app/second-page/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function SecondPage() {
+  return (
+    <div>
+      <h1>Second PAGe! yo</h1>
+      <Link href="/">Back to home!</Link>
+    </div>
+  )
+}
